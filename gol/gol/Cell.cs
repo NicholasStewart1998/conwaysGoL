e4e87920722aeb0d2bb5b9ef8d4990a32cb5d7f0
NicholasStewart1998 <NicholasStewart5239@gmail.com>
2017-08-08 16:25:59 +1000

    Add files via upload

---
+++ gol/gol/Cell.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace gameOfLife {
+
+    class Cell : PictureBox {
+
+        private int cellX, cellY;
+        private bool state;
+
+        public Cell(int x, int y, int xPos, int yPos, int width, int height) {
+            this.SetBounds(xPos, yPos, width, height);
+            cellX = x;
+            cellY = y;
+            this.BackColor = Color.Aquamarine;
+        }
+
+        public bool alive {
+            get { return state; }
+            set {
+                state = value;
+                if (state) {
+                    this.BackColor = Color.Blue;
+                } else {
+                    this.BackColor = Color.Aquamarine;
+                }
+            }
+        }
+
+        public int row {
+            get { return cellX; }
+            set { cellX = value; }
+        }
+
+        public int col {
+            get { return cellY; }
+            set { cellY = value; }
+        }
+
+    }
+}
---
+++ gol/gol/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace gameOfLife {
+    public partial class Form1 : Form {
+        private const int cellsX = 30, cellsY = 30, cellH = 15, cellW = 15, cellSp = 3, outerSpacing = 14;
+        public Form1() {
+            InitializeComponent();
+            //this.SetBounds(0 - cellSpacing, 0 - cellSpacing, (cellWidth + cellSpacing) * cellsX, (cellHeight + cellSpacing) * cellsY);
+            this.Height = SystemInformation.CaptionHeight + outerSpacing + 2 * cellSp + (cellH + cellSp) * cellsY;
+            this.Width = outerSpacing + 2 * cellSp + (cellW + cellSp) * cellsX;
+            setupCells(cellsX, cellsY, cellW, cellH, cellSp);
+            
+        }
+
+        private void setupCells(int numCellsX, int numCellsY, int cellWidth, int cellHeight, int spacing) {
+            List<List<Cell>> cellX = new List<List<Cell>>();
+            for (int i = 0; i < numCellsX; i++) {
+                List<Cell> cellY = new List<Cell>();
+                for (int j = 0; j < numCellsY; j++) {
+                    Cell cell = new Cell(i, j, spacing + (spacing + cellWidth) * i, spacing + (spacing + cellHeight) * j, cellWidth, cellHeight);
+                    cellY.Add(cell);
+                    cell.MouseDown += new MouseEventHandler(cellClickHander);
+                    this.Controls.Add(cell);
+                    Console.WriteLine(spacing + (spacing + cellHeight) * j);
+                }
+                cellX.Add(cellY);
+            }
+        }
+
+        private void setupForm() {
+
+        }
+
+        private void cellClickHander(object sender, MouseEventArgs e) {
+            Cell cell = (Cell)sender;
+
+            if(cell.alive) {
+                cell.alive = false;
+            } else {
+                cell.alive = true;
+            }
+        }
+
+    }
+}
